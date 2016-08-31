a324d55eb6b3852cfe9813d9c4f8b47dce6491f8
Dandan Wei <dandan.wei@ymail.com>
2016-08-31 23:04:30 +0200

    bump a dumy version

--- vendor/assets/javascripts/kindeditor/kindeditor.js
+++ vendor/assets/javascripts/kindeditor/kindeditor.js
@@ -17,7 +17,7 @@ if (!window.console) {
 if (!console.log) {
 	console.log = function () {};
 }
-var _VERSION = '4.1.10.4 (2016-08-31)',
+var _VERSION = '4.1.10.5 (2016-08-31)',
 	_ua = navigator.userAgent.toLowerCase(),
 	_IE = _ua.indexOf('msie') > -1 && _ua.indexOf('opera') == -1,
 	_NEWIE = _ua.indexOf('msie') == -1 && _ua.indexOf('trident') > -1,
